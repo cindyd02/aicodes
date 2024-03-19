2b98a00bf65aa07efea40e61e394f2f389198456
cindyd02 <127247829+cindyd02@users.noreply.github.com>
2024-03-18 22:24:58 -0400

    Updated about.html

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function toggleMenu() {
     var menuItems = document.getElementById("menuItems");
     menuItems.classList.toggle("active");
 }
+
+// Function to hide the menu when tapping outside of it
+document.addEventListener('click', function(event) {
+    var menuItems = document.getElementById("menuItems");
+    var menuToggle = document.querySelector('.menu-toggle');
+    if (menuItems.classList.contains('active') && !menuToggle.contains(event.target)) {
+        menuItems.classList.remove('active');
+    }
+});
+
+// Function to hide the menu when scrolling
+window.addEventListener('scroll', function() {
+    var menuItems = document.getElementById("menuItems");
+    if (menuItems.classList.contains('active')) {
+        menuItems.classList.remove('active');
+    }
+});
